bd9a43763162d5fbd89a493d29dcfef92d960c17
cassiojuniorr <cassiospcjunior@gmail.com>
2022-11-22 17:48:55 -0300

    model and migration from user right

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,38 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+export default class UserModel extends Model {
+  declare id: number;
+  declare username: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  modelName: 'UserModel',
+});
